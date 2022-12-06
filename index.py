8510f513daf5923027af0eb999063e16e752679a
sjetp <sjetpaul23@gmail.com>
2022-12-06 18:53:53 +0530

    Trying form

--- index.py
+++ index.py
@@ -6,6 +6,7 @@ with st.form("my_form"):
    slider_val2 = st.number_input("Insert a Number")
    checkbox_val = st.checkbox("Form checkbox")
    slider_val=slider_val1 + slider_val2
+   
    # Every form must have a submit button.
    submitted = st.form_submit_button("Submit")
    if submitted:
