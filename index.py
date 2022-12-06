c924e3a823ce9b37d5433b6cd4f7f3c6c01cf0ce
sjetp <sjetpaul23@gmail.com>
2022-12-06 18:55:21 +0530

    Trying form

--- index.py
+++ index.py
@@ -3,10 +3,10 @@ st.write("First Web App")
 with st.form("my_form"):
    st.write("Inside the form")
    slider_val1 = st.number_input("Insert a Number")
-   slider_val2 = st.number_input("Insert a Number")
+   slider_val2 = st.number_input("Insert another Number")
    checkbox_val = st.checkbox("Form checkbox")
    slider_val=slider_val1 + slider_val2
-   
+
    # Every form must have a submit button.
    submitted = st.form_submit_button("Submit")
    if submitted:
