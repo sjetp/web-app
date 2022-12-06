befad8eab87679f9101a0989e9c384b77bca2aa4
sjetp <sjetpaul23@gmail.com>
2022-12-06 18:52:39 +0530

    Trying form

--- index.py
+++ index.py
@@ -2,9 +2,10 @@ import streamlit as st
 st.write("First Web App")
 with st.form("my_form"):
    st.write("Inside the form")
-   slider_val = st.slider("Form slider")
+   slider_val1 = st.number_input("Insert a Number")
+   slider_val2 = st.number_input("Insert a Number")
    checkbox_val = st.checkbox("Form checkbox")
-
+   slider_val=slider_val1 + slider_val2
    # Every form must have a submit button.
    submitted = st.form_submit_button("Submit")
    if submitted:
